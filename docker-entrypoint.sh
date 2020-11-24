8a0cbfc16605b41987f76182663ea98be3822369
taeju.kim <taeju.kim@nhntoast.com>
2020-11-24 22:12:09 +0900

    change docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-echo "Makemigragions"
+echo ":::::::Makemigragions"
 python manage.py makemigrations
 python manage.py migrate
 
-echo "Collect static files"
+echo ":::::::Collect static files"
 python manage.py collectstatic --noinput
 
-echo "Run gunicorn"
-gunicorn -b 0:4000 -w 2 sm.wsgi:application
-
-echo "Create supser user"
+echo ":::::::Create supser user"
 echo "from django.contrib.auth.models import User;\
       User.objects.create_superuser('$DJANGO_SU_NAME', \
       '$DJANGO_SU_EMAIL', '$DJANGO_SU_PASSWORD')" | python manage.py shell
+      
+echo ":::::::Run gunicorn"
+gunicorn -b 0:4000 -w 2 sm.wsgi:application
+
