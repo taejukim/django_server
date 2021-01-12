8424543e655d9750d4c15b3ee2847ba635bae712
taeju.kim <taeju.kim@nhntoast.com>
2021-01-12 17:34:23 +0900

    add csrf exampt

--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http.response import JsonResponse
 from datetime import datetime
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@csrf_exempt
 def method_test(request):
     now = datetime.now().isoformat()
     retv = {
