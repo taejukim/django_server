c109777476fbc648e12f73ce7956eb7b23a5072b
taeju.kim <taeju.kim@nhntoast.com>
2021-02-17 10:14:49 +0900

    change get_ssl.sh

--- get_ssl.sh
+++ get_ssl.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains="api-test-server.ml" 
+domains="*.apitests.ml" 
 rsa_key_size=4096
 data_path="./certbot"
 email="taeju.kim@nhnsoft.com" # Adding a valid address is strongly recommended
