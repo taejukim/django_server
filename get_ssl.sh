9915b9d10e87e8414befa34abce16c6de4bd0fd1
taeju.kim <taeju.kim@nhnsoft.com>
2022-03-06 18:00:41 +0900

    도메인 변경(api-ht.ml > mock.domain-for.test.com)

--- get_ssl.sh
+++ get_ssl.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains="api-ht.ml" 
+domains="mock.domain-for-test.com" 
 rsa_key_size=4096
 data_path="./certbot"
 email="taeju.kim@nhnsoft.com" # Adding a valid address is strongly recommended
