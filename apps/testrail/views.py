7dcca6117ff4a7cf219a328976e80fc2441fa23c
taeju.kim <taeju.kim@nhnenter.com>
2023-02-06 16:52:54 +0900

    return json file

--- apps/testrail/views.py
+++ apps/testrail/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from apps.testrail.models import RequestInfo
+import os
+from django_server import settings
 
 @csrf_exempt
 def request_info(request, url_path=None):
@@ -14,68 +16,6 @@ def request_info(request, url_path=None):
     return HttpResponse(url_path)
 
 def trackers(request):
-    data = {
-        "trackers": [
-            {
-                "id": 1,
-                "name": "결함",
-                "default_status": {
-                    "id": 1,
-                    "name": "신규"
-                },
-                "description": None,
-                "enabled_standard_fields": [
-                    "assigned_to_id",
-                    "category_id",
-                    "fixed_version_id",
-                    "parent_issue_id",
-                    "start_date",
-                    "due_date",
-                    "estimated_hours",
-                    "done_ratio",
-                    "description"
-                ]
-            },
-            {
-                "id": 2,
-                "name": "새기능",
-                "default_status": {
-                    "id": 1,
-                    "name": "신규"
-                },
-                "description": None,
-                "enabled_standard_fields": [
-                    "assigned_to_id",
-                    "category_id",
-                    "fixed_version_id",
-                    "parent_issue_id",
-                    "start_date",
-                    "due_date",
-                    "estimated_hours",
-                    "done_ratio",
-                    "description"
-                ]
-            },
-            {
-                "id": 3,
-                "name": "지원",
-                "default_status": {
-                    "id": 1,
-                    "name": "신규"
-                },
-                "description": None,
-                "enabled_standard_fields": [
-                    "assigned_to_id",
-                    "category_id",
-                    "fixed_version_id",
-                    "parent_issue_id",
-                    "start_date",
-                    "due_date",
-                    "estimated_hours",
-                    "done_ratio",
-                    "description"
-                ]
-            }
-        ]
-    }
-    return JsonResponse(data)
+    backup_path = './apps/testrail/trackers.json'
+    return HttpResponse(open(backup_path, 'r'),content_type = 'application/json; charset=utf8')
+    
