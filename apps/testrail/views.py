908474a613a66d6bca607bb89f4e3c68d64e976f
taeju.kim <taeju.kim@nhnenter.com>
2023-02-07 09:40:34 +0900

    change url parsing rule

--- apps/testrail/views.py
+++ apps/testrail/views.py
@@ -8,13 +8,13 @@ from django_server import settings
 @csrf_exempt
 def request_info(request, url_path=None):
     path = request.path
-    if url_path in "trackers.json":
+    if "trackers.json" in url_path:
         backup_path = './apps/testrail/trackers.json'
         return HttpResponse(open(backup_path, 'r'),content_type = 'application/json; charset=utf8')
-    elif url_path in "projects.json":
+    elif "projects.json" in url_path:
         backup_path = './apps/testrail/projects.json'
         return HttpResponse(open(backup_path, 'r'),content_type = 'application/json; charset=utf8')
-    elif url_path in 'issue_categories.json':
+    elif 'issue_categories.json' in url_path:
         backup_path = './apps/testrail/issue_categories.json'
         return HttpResponse(open(backup_path, 'r'),content_type = 'application/json; charset=utf8')
     else:
